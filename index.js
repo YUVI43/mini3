6e74d5124943face51db73a366472551e1c4080d
BabaAayam <aayambajaj4@gmail.com>
2024-09-27 10:43:42 +0530

    a

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(4000, () => {
-    console.log('Serving on port 4000')
+app.listen(5000, () => {
+    console.log('Serving on port 5000')
 })
