359115d231a79de9080b9bf6c502a69a3ce3b2f2
YUVI43 <127021768+YUVI43@users.noreply.github.com>
2024-10-03 01:13:01 +0530

    Update review.js

--- models/review.js
+++ models/review.js
@@ -1,12 +1,13 @@
-const { number } = require('joi')
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     body: String,
-    rating: Number
-})
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
 
-module.exports = mongoose.model("Review", reviewSchema)
-// new model in the mongoose created with the name =>Review , i.e reviews table created in the mongoose
+module.exports = mongoose.model("Review", reviewSchema);
